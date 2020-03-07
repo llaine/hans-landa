33c32b1accee0f1f762d01d6ec103b08bee8f511
Louis Lainé <darksioul6@gmail.com>
2020-03-07 17:26:01 +0100

    --wip-- [skip ci]

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -2,7 +2,19 @@
 import {context} from '@actions/github'
 
 export function shouldTriggerBuild(): boolean {
-  console.log(context.payload)
+  // const octokit = new GitHub('503d8295502e1b9ec9b88496c436c3d494bcdd55')
+  // eslint-disable-next-line no-empty-pattern
+  const {
+    // repo: {owner, repo},
+  } = context
+
+  console.log(context.payload.repository)
+
+  // const pr = octokit.pulls.get({
+  //   owner,
+  //   repo,
+  //   pull_number
+  // });
 
   return true
 }
