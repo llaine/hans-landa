3824f0d430344985372986fe221db643b83f1392
ahanriat <ahanriat@gmail.com>
2020-03-07 16:51:57 +0100

    Implement triggerBitriseBuild

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import * as core from '@actions/core'
+import { triggerBuild } from './utils/bitrise-utils'
+
+// import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   // 1 recup params app_slug & build_trigger_token & github_token
@@ -6,6 +8,15 @@ async function run(): Promise<void> {
   // check if we want to trigger the build //tbd
 
   // trigger the build
+
+  triggerBuild({
+    bitriseAppSlug: 'ca04e0425716e1ce',
+    bitriseWorkflow: 'primary',
+    bitriseBuildTriggerToken: 'Gs7vZtsWEz-UZ_vNdNxDIA',
+    branchName: 'implement-bitrise-utils',
+    commitHash: 'fb96e1ed33612afb5190156fc1ce53922fd2609a',
+    pullRequestId: 10,
+  })
 }
 
 run()
--- src/utils/bitrise-utils.ts
+++ src/utils/bitrise-utils.ts
@@ -1,5 +1,73 @@
-type ExtraGithubInfo = any;
+import Axios from 'axios'
 
-export function triggerBuild(params: { bitriseBuildTriggerToken: string, bitriseAppSlug: string, githubInfo: ExtraGithubInfo}): Promise<void> {
-  // TODO
+export async function triggerBuild({
+  bitriseBuildTriggerToken,
+  bitriseAppSlug,
+  bitriseWorkflow,
+  branchName,
+  commitHash,
+  pullRequestId,
+}: {
+  bitriseBuildTriggerToken: string
+  bitriseAppSlug: string
+  bitriseWorkflow: string
+  branchName: string
+  commitHash: string
+  pullRequestId?: number
+}): Promise<BitriseResponse> {
+  console.log(
+    `Triggering Bitrise workflow ${bitriseWorkflow} on branch ${branchName} for app slug ${bitriseAppSlug}...`,
+  )
+  return Axios.post(
+    `https://app.bitrise.io/app/${bitriseAppSlug}/build/start.json`,
+    {
+      hook_info: {
+        type: 'bitrise',
+        build_trigger_token: bitriseBuildTriggerToken,
+      },
+      build_params: {
+        branch: branchName,
+        workflow_id: bitriseWorkflow,
+        commit_hash: commitHash,
+        pull_request_id: pullRequestId,
+      } as BitriseBuildParams,
+      triggered_by: 'curl',
+    },
+  ).then(result => {
+    const data = result.data as BitriseResponse
+    console.log(`Build url: ${data.build_url}`)
+    return data
+  })
+}
+
+// More info here https://api-docs.bitrise.io/#/builds/build-trigger
+interface BitriseBuildParams {
+  branch: string
+  workflow_id: string
+
+  branch_dest?: string
+  branch_dest_repo_owner?: string
+  branch_repo_owner?: string
+  build_request_slug?: string
+  commit_hash?: string
+  commit_message?: string
+  diff_url?: string
+  pull_request_author?: string
+  pull_request_head_branch?: string
+  pull_request_id?: number
+  pull_request_merge_branch?: string
+  pull_request_repository_url?: string
+  skip_git_status_report?: boolean
+  tag?: string
+}
+
+interface BitriseResponse {
+  status: string // 'ok' when succeed
+  message: string
+  slug: string
+  service: 'bitrise'
+  build_slug: string
+  build_number: number
+  build_url: string
+  triggered_workflow: string
 }
