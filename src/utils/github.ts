a196b6e00749f0ebc1814c430d6366c855698770
Louis Lainé <darksioul6@gmail.com>
2020-03-07 17:52:11 +0100

    --wip-- [skip ci]

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -1,18 +1,22 @@
-import { context } from '@actions/github'
+import { context, GitHub } from '@actions/github'
 
-export function shouldTriggerBuild(): boolean {
-  // const octokit = new GitHub('503d8295502e1b9ec9b88496c436c3d494bcdd55')
-  // const {
-  // repo: {owner, repo},
-  // } = context
+export async function shouldTriggerBuild(): Promise<boolean> {
+  const octokit = new GitHub('503d8295502e1b9ec9b88496c436c3d494bcdd55')
+  const {
+    repo: { owner, repo },
+    payload,
+  } = context
 
-  console.log(context.payload.pull_request)
+  if (!payload.pull_request) return true
 
-  // const pr = octokit.pulls.get({
-  //   owner,
-  //   repo,
-  //   pull_number
-  // });
+  const pull_number = payload.pull_request.number
 
+  const pr = await octokit.pulls.listReviews({
+    owner,
+    repo,
+    pull_number,
+  })
+
+  console.log(pr.data)
   return true
 }
