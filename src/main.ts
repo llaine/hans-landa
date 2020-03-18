30963755ac5b242f7652cb8165df0bbdf4d640f5
Louis Lainé <darksioul6@gmail.com>
2020-03-18 17:50:27 +0100

    feat: catching more informations

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import * as core from '@actions/core'
 import { triggerBuild } from './utils/bitrise-utils'
-import { context } from '@actions/github'
+import { context, GitHub } from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    console.log(context.payload.comment)
-    console.log(context.payload.pull_request)
     if (context.payload.comment && context.payload.issue) {
+      const client = new GitHub(core.getInput('github_token'))
+      const prInformations = context.payload.issue.pull_request.url.split('/')
+      const prNumber = prInformations.pop()
+      const owner = prInformations[4]
+      const repo = prInformations[5]
+      const pr = await client.pulls.get({ pull_number: prNumber, owner, repo })
+      console.log(pr);
       console.log(
         context.payload.comment.body,
         ' comment on ',
