bc48d98eda93d68577c4ceaf88057872a4236eff
Louis Lainé <darksioul6@gmail.com>
2020-03-18 16:50:58 +0100

    --wip-- [skip ci]

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,35 @@ import { context } from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const bitriseAppSlug = core.getInput('bitrise_app_slug')
-    const bitriseBuildTriggerToken = core.getInput(
-      'bitrise_build_trigger_token',
-    )
-    const bitriseWorkflow = core.getInput('bitrise_workflow')
-    let branchName = context.ref.slice(11)
-    let commitHash = context.sha
+    console.log('is issue', context.payload.issue)
+    console.log('is pr', context.payload.pull_request)
+    if (context.payload.issue) {
+      console.log(context.payload.issue)
+    } else {
+      const bitriseAppSlug = core.getInput('bitrise_app_slug')
+      const bitriseBuildTriggerToken = core.getInput(
+        'bitrise_build_trigger_token',
+      )
+      const bitriseWorkflow = core.getInput('bitrise_workflow')
+      let branchName = context.ref.slice(11)
+      let commitHash = context.sha
 
-    if (context.payload.pull_request) {
-      branchName = context.payload.pull_request.head.ref
-      commitHash = context.payload.pull_request.head.sha
+      if (context.payload.pull_request) {
+        branchName = context.payload.pull_request.head.ref
+        commitHash = context.payload.pull_request.head.sha
+      }
+
+      triggerBuild({
+        bitriseAppSlug,
+        bitriseWorkflow,
+        bitriseBuildTriggerToken,
+        branchName,
+        commitHash,
+        commitMessage: context.payload.pull_request?.body,
+        pullRequestId: context.payload.pull_request?.number,
+        branchDestName: context.payload.pull_request?.base.ref,
+      })
     }
-
-    triggerBuild({
-      bitriseAppSlug,
-      bitriseWorkflow,
-      bitriseBuildTriggerToken,
-      branchName,
-      commitHash,
-      commitMessage: context.payload.pull_request?.body,
-      pullRequestId: context.payload.pull_request?.number,
-      branchDestName: context.payload.pull_request?.base.ref,
-    })
   } catch (error) {
     core.setFailed(error.message)
   }
